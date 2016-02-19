6deec473c3f3774b030cb814c0b9c085e4a45c0c
Josh <joshoverly@yahoo.com>
2016-02-19 08:32:10 -0800

    fail second spec

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -2,12 +2,23 @@
     require_once "src/RepeatCounter.php";
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_setMonthOne()
+        function test_oneLetter()
         {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word_searched = "a";
             $string_searched = "a";
+            //Act: runs the actual method that we are testing.
+            $result = $test_RepeatCounter->countRepeats($word_searched,$string_searched);
+            //Assert tells our tests what to expect from the output of our method.
+            $this->assertEquals(1, $result);
+        }
+        function test_setMonthOne()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $word_searched = "a";
+            $string_searched = "b";
             //Act: runs the actual method that we are testing.
             $result = $test_RepeatCounter->countRepeats($word_searched,$string_searched);
             //Assert tells our tests what to expect from the output of our method.
