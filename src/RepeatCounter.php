7ef2b1f5e6b80f53ba11d6f7e8e632e5e9eec1b0
Josh <joshoverly@yahoo.com>
2016-02-19 08:27:42 -0800

    first spec passes

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,9 +1,16 @@
 <?php
 class RepeatCounter
 {
-    function countRepeats()
+    function countRepeats($word_searched, $string_searched)
     {
-
+        $string_words = explode(" ", $string_searched);
+        $count = 0;
+        foreach ($string_words as $string_word) {
+            if ($word_searched == $string_word) {
+                $count++;
+            }
+        }
+        return $count;
     }
 }
 
